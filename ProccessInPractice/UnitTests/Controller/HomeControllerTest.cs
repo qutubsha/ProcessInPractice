655da141c020a0a504390b7c01137ac0c424b156
SIGMAINFOAHD\kutbuddin.s <kutbuddin.s@sigmainfo.net>
2018-10-08 16:58:57 +0530

    Unit Test in same project

---
+++ ProccessInPractice/UnitTests/Controller/HomeControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using ProccessInPractice.Controllers;
+using System.Web.Mvc;
+
+namespace ProccessInPractice.UnitTests.Controller
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("This is My New Home Page Again", result.ViewBag.Title);
+        }
+    }
+}
