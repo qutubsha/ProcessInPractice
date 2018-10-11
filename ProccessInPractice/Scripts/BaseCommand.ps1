8d04a00995d14291bb1d08851f20d441f597d32a
SIGMAINFOAHD\kutbuddin.s <kutbuddin.s@sigmainfo.net>
2018-10-11 15:29:21 +0530

    Changes

--- ProccessInPractice/Scripts/BaseCommand.ps1
+++ ProccessInPractice/Scripts/BaseCommand.ps1
@@ -17,8 +17,8 @@
 #    process
 #    {
         $msBuildExe = 'C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe'
-        #$path = 'Freestyle_ProcessInPractice/ProccessInPractice.sln'
-		$path = 'D:\Process_In_Practice\Source\ProccessInPractice.sln'
+        $path = 'Freestyle_ProcessInPractice/ProccessInPractice.sln'
+		#$path = 'D:\Process_In_Practice\Source\ProccessInPractice.sln'
 
         #if($nuget){
             Write-Host "Restoring NuGet packages" -ForegroundColor Green
