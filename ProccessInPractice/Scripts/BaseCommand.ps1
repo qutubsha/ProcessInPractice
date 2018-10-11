b38bf459285551f312733cad0990e99ea4877489
SIGMAINFOAHD\kutbuddin.s <kutbuddin.s@sigmainfo.net>
2018-10-11 12:00:15 +0530

    PowerShell Script Added

---
+++ ProccessInPractice/Scripts/BaseCommand.ps1
@@ -0,0 +1,35 @@
+#
+# BaseCommand.ps1
+#
+function buildVS
+{
+    # param
+    # (
+    #     [parameter(Mandatory=$true)]
+    #     [String] $path,
+
+    #     [parameter(Mandatory=$false)]
+    #     [bool] $nuget = $true,
+
+    #     [parameter(Mandatory=$false)]
+    #     [string] $clean = $true
+    # )
+    process
+    {
+        $msBuildExe = 'C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe'
+        $path = 'Freestyle_ProcessInPractice/ProccessInPractice.sln'
+
+        if($nuget){
+            Write-Host "Restoring NuGet packages" -ForegroundColor Green
+            nuget restore "$($path)"
+        }
+
+        if($clean){
+            Write-Host "Cleaning $($path)" -ForegroundColor Green
+            & "$($msBuildExe)" "$($path)" /t:Clean /m
+        }
+
+        Write-Host "Building $($path)" -ForegroundColor Green
+        & "$($msBuildExe)" "$($path)" /t:Build /m
+    }
+}
