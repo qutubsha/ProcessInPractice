893a3813275c9783a243e4cc398f6bede2ca64f1
SIGMAINFOAHD\kutbuddin.s <kutbuddin.s@sigmainfo.net>
2018-10-11 12:34:42 +0530

    Push Again

--- ProccessInPractice/Scripts/BaseCommand.ps1
+++ ProccessInPractice/Scripts/BaseCommand.ps1
@@ -1,35 +1,36 @@
 #
 # BaseCommand.ps1
 #
-function buildVS
-{
-    # param
-    # (
-    #     [parameter(Mandatory=$true)]
-    #     [String] $path,
+#function buildVS
+#{
+#    # param
+#    # (
+#    #     [parameter(Mandatory=$true)]
+#    #     [String] $path,
 
-    #     [parameter(Mandatory=$false)]
-    #     [bool] $nuget = $true,
+#    #     [parameter(Mandatory=$false)]
+#    #     [bool] $nuget = $true,
 
-    #     [parameter(Mandatory=$false)]
-    #     [string] $clean = $true
-    # )
-    process
-    {
+#    #     [parameter(Mandatory=$false)]
+#    #     [string] $clean = $true
+#    # )
+#    process
+#    {
         $msBuildExe = 'C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe'
-        $path = 'Freestyle_ProcessInPractice/ProccessInPractice.sln'
+        #$path = 'Freestyle_ProcessInPractice/ProccessInPractice.sln'
+		$path = 'D:\Process_In_Practice\Source\ProccessInPractice.sln'
 
-        if($nuget){
+        #if($nuget){
             Write-Host "Restoring NuGet packages" -ForegroundColor Green
             nuget restore "$($path)"
-        }
+        #}
 
-        if($clean){
+        #if($clean){
             Write-Host "Cleaning $($path)" -ForegroundColor Green
             & "$($msBuildExe)" "$($path)" /t:Clean /m
-        }
+        #}
 
         Write-Host "Building $($path)" -ForegroundColor Green
         & "$($msBuildExe)" "$($path)" /t:Build /m
-    }
-}
+#    }
+#}
